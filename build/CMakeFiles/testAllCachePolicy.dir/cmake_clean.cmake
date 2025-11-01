945d2a05cab6b226f064b4ba5c2f8476c145bdb0
RY-Xin <xinhuiyu0216@gmail.com>
2025-11-01 15:06:12 -0400

    Add build system and cache policy test results

---
+++ build/CMakeFiles/testAllCachePolicy.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/testAllCachePolicy.dir/testAllCachePolicy.cpp.o"
+  "CMakeFiles/testAllCachePolicy.dir/testAllCachePolicy.cpp.o.d"
+  "testAllCachePolicy"
+  "testAllCachePolicy.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/testAllCachePolicy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/testAllCachePolicy.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for testAllCachePolicy.
+# This may be replaced when dependencies are built.
